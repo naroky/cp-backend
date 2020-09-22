ee6a8b07c382d09698c10f25e6befb841b773305
TRUEDIGITAL\narupol.bal <narupol.b@gmail.com>
2020-09-22 17:11:07 +0700

    callback:Development complete.

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-class CallbackController extends Controller
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+class CallbackController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -13,7 +14,25 @@ use Illuminate\Http\Request;
 
     public function callback(Request $request)
     {
-        echo "callback";
+        $request_data = $request->getContent();
+        $obj_request = json_decode($request_data);
+        $trans_id = $obj_request->data->trans_id;
+        /*
+        $obj_request->data->cp_trans_id
+        $obj_request->data->msisdn
+        $obj_request->data->shortcode
+        $obj_request->data->service_id
+        $obj_request->data->css_keyword
+        $obj_request->data->cp_id
+        */
+        $statusCode=200;
+        $jsonRes = new \stdClass();
+        $jsonRes->code = $statusCode;
+        $jsonRes->description = "Success";
+        $jsonRes->trans_id =$trans_id;
+        $content = json_encode($jsonRes);
+        return (new Response($content, $statusCode))
+        ->header('Content-Type', 'Application/json');
     }
     //
 }
